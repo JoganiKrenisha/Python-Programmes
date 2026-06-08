80674662a21a184c6581ead2b55bbac67a9418fa
Jogani Krenisha L <25dcs042@charusat.edu.in>
2026-06-08 19:22:43 +0530

    Calculator
    It is create using simple Operators

---
+++ 1.py
@@ -0,0 +1,7 @@
+a=80
+b=27
+
+print("The value of ", a , "+", b ,"is :",a+b)
+print("The value of ", a , "-", b ,"is :",a-b)
+print("The value of ", a , "*", b ,"is :",a*b)
+print("The value of ", a , "/", b ,"is :",a/b)
